6b117577c93f9bac7dfdc6a3f67b9a8a8e1f7e84
Simona Simeonova <Sim0o0na@abv.bg>
2016-11-30 01:57:36 +0200

    Added See Full link, header changes

--- src/Views/allOffers.js
+++ src/Views/allOffers.js
@@ -8,60 +8,71 @@ export default class AllOffersGrid extends React.Component {
     const mediumDivStyle = {height:'170px'};
         return(
             <div id="grid_container">
-                <div style={bigDivStyle} onClick={this.props.offerClicked}>
+                <div style={bigDivStyle} >
                     <img className="offerImg" src='http://bit.ly/2fJ41F7' alt="ClothingImg" />
                     <p className="offerTitle">Offer Title</p>
                     <p className="offerPrice">Offer Price</p>
+                    <p onClick={this.props.offerClicked} className="toFullLink">See Full</p>
                 </div>
-                <div style={bigDivStyle} onClick={this.props.offerClicked}>
+                <div style={bigDivStyle}>
                     <img className="offerImg" src='http://bit.ly/2fO4V6l' alt="ClothingImg" />
                     <p className="offerTitle">Offer Title</p>
                     <p className="offerPrice">Offer Price</p>
+                    <p onClick={this.props.offerClicked} className="toFullLink">See Full</p>
                 </div>
-                <div style={smallDivStyle} onClick={this.props.offerClicked}>
+                <div style={smallDivStyle}>
                     <img className="offerImg" src='http://bit.ly/2fJbdAJ' alt="ClothingImg" />
                     <p className="offerTitle">Offer Title</p>
                     <p className="offerPrice">Offer Price</p>
+                    <p onClick={this.props.offerClicked} className="toFullLink">See Full</p>
                 </div>
-                <div style={bigDivStyle} onClick={this.props.offerClicked}>
+                <div style={bigDivStyle}>
                     <img className="offerImg" src='http://bit.ly/2fO4V6l' alt="ClothingImg" />
                     <p className="offerTitle">Offer Title</p>
                     <p className="offerPrice">Offer Price</p>
+                    <p onClick={this.props.offerClicked} className="toFullLink">See Full</p>
                 </div>
-                <div style={mediumDivStyle} onClick={this.props.offerClicked}>
+                <div style={mediumDivStyle} >
                     <img className="offerImg" src='http://bit.ly/2fO4V6l' alt="ClothingImg" />
                     <p className="offerTitle">Offer Title</p>
                     <p className="offerPrice">Offer Price</p>
+                    <p onClick={this.props.offerClicked} className="toFullLink">See Full</p>
                 </div>
-                <div style={mediumDivStyle} onClick={this.props.offerClicked}>
+                <div style={mediumDivStyle} >
                     <img className="offerImg" src='http://bit.ly/2fO4V6l' alt="ClothingImg" />
                     <p className="offerTitle">Offer Title</p>
                     <p className="offerPrice">Offer Price</p>
+                    <p onClick={this.props.offerClicked} className="toFullLink">See Full</p>
                 </div>
-                <div style={bigDivStyle} onClick={this.props.offerClicked}>
+                <div style={bigDivStyle} >
                     <img className="offerImg" src='http://bit.ly/2fO4V6l' alt="ClothingImg" />
                     <p className="offerTitle">Offer Title</p>
                     <p className="offerPrice">Offer Price</p>
+                    <p onClick={this.props.offerClicked} className="toFullLink">See Full</p>
                 </div>
-                <div style={mediumDivStyle} onClick={this.props.offerClicked}>
+                <div style={mediumDivStyle} >
                     <img className="offerImg" src='http://bit.ly/2fO4V6l' alt="ClothingImg" />
                     <p className="offerTitle">Offer Title</p>
                     <p className="offerPrice">Offer Price</p>
+                    <p onClick={this.props.offerClicked} className="toFullLink">See Full</p>
                 </div>
-                <div style={mediumDivStyle} onClick={this.props.offerClicked}>
+                <div style={mediumDivStyle} >
                     <img className="offerImg" src='http://bit.ly/2fO4V6l' alt="ClothingImg" />
                     <p className="offerTitle">Offer Title</p>
                     <p className="offerPrice">Offer Price</p>
+                    <p onClick={this.props.offerClicked} className="toFullLink">See Full</p>
                 </div>
-                <div style={bigDivStyle} onClick={this.props.offerClicked}>
+                <div style={bigDivStyle}>
                     <img className="offerImg" src='http://bit.ly/2fO4V6l' alt="ClothingImg" />
                     <p className="offerTitle">Offer Title</p>
                     <p className="offerPrice">Offer Price</p>
+                    <p onClick={this.props.offerClicked} className="toFullLink">See Full</p>
                 </div>
-                <div style={smallDivStyle} onClick={this.props.offerClicked}>
+                <div style={smallDivStyle}>
                     <img className="offerImg" src='http://bit.ly/2fO4V6l' alt="ClothingImg" />
                     <p className="offerTitle">Offer Title</p>
                     <p className="offerPrice">Offer Price</p>
+                    <p onClick={this.props.offerClicked} className="toFullLink">See Full</p>
                 </div>
             </div>
         )
