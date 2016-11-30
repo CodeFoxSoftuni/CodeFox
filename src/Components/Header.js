2a068f2bd46525b9ba5354711bb682e08373d12b
Plamen Dimitrov <pl.st.dimitrov@gmail.com>
2016-11-30 05:10:52 +0200

    Small modifications.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,25 +3,27 @@ import '../Styles/Header-Styles.css'
 
 export default class Header extends React.Component {
     render() {
-            let username = this.props.username;
-            if(username == null){
-                return (
+        let username = this.props.username;
+        if (username == null) {
+            return (
                 <div className="nav-bar">
-                    <img src='https://s21.postimg.org/hbxdgil47/2f1e3e17638437_562bc8a645054.png' role="presentation" alt="logo"/>
+                    <img src='https://s21.postimg.org/hbxdgil47/2f1e3e17638437_562bc8a645054.png' role="presentation"
+                         alt="logo"/>
                     <a href="#" className="nav-bar-link" onClick={this.props.homeClicked}>Home</a>
                     <a href="#" className="nav-bar-link" onClick={this.props.loginClicked}>Login</a>
                     <a href="#" className="nav-bar-link" onClick={this.props.registerClicked}>Register</a>
                     <a href="#" className="nav-bar-link" onClick={this.props.allOffersClicked}>All offers</a>
                 </div>
-                )} else {
-                return (
-                    <div className="nav-bar">
-                        <img src="img/logo.png" alt="logo" />
-                        <a href="#" className="nav-bar-link" onClick={this.props.homeClicked}>Home</a>
-                        <a href="#" className="nav-bar-link" onClick={this.props.LogoutClicked}>Logout</a>
-                        <a href="#" className="nav-bar-link" onClick={this.props.allOffersClicked}>All offers</a>
-                    </div>
-                )
-            }
+            )
+        } else {
+            return (
+                <div className="nav-bar">
+                    <img src="https://s21.postimg.org/hbxdgil47/2f1e3e17638437_562bc8a645054.png" alt="logo"/>
+                    <a href="#" className="nav-bar-link" onClick={this.props.homeClicked}>Home</a>
+                    <a href="#" className="nav-bar-link" onClick={this.props.logoutClicked}>Logout</a>
+                    <a href="#" className="nav-bar-link" onClick={this.props.allOffersClicked}>All offers</a>
+                </div>
+            )
+        }
     }
 }
