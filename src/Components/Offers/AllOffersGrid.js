ec5ddbcc911760a619e1d0207b03463fc28fc682
Nikolay <rogchev@gmail.com>
2016-12-06 16:53:00 +0200

    Category queries

--- src/Components/Offers/AllOffersGrid.js
+++ src/Components/Offers/AllOffersGrid.js
@@ -9,16 +9,20 @@ export default class AllOffersGrid extends React.Component {
         $('#center').css('margin-left', divWidth + 'em');
     }
 
+    categoryClickHandler() {
+        this.props.renderCategory(this.name); // this.name is the name of the clicked category
+    }
+
     render() {
         return (
             <div id="container">
                 <section id="left" className="categoriesMenu">
                     <div id='section-label' className="category-link">CATEGORIES</div>
-                    <Link to="/category/All" className="category-link">All</Link>
-                    <Link to="/category/Lingerie" className="category-link">Lingerie</Link>
-                    <Link to="/category/Bottoms" className="category-link">Bottoms</Link>
-                    <Link to="/category/Shirts, Blouses etc." className="category-link">Shirts, Blouses etc.</Link>
-                    <Link to="/category/Sport wear" className="category-link">Sport wear</Link>
+                    <Link to="/category/All" className="category-link" onClick={this.categoryClickHandler}>All</Link>
+                    <Link to="/category/Lingerie" className="category-link" onClick={this.categoryClickHandler} name="Lingerie">Lingerie</Link>
+                    <Link to="/category/Bottoms" className="category-link" onClick={this.categoryClickHandler} name="Bottoms">Bottoms</Link>
+                    <Link to="/category/Shirts-Blouses-etc." className="category-link" onClick={this.categoryClickHandler} name="Top">Shirts, Blouses etc.</Link>
+                    <Link to="/category/Sport wear" className="category-link" onClick={this.categoryClickHandler} name="Sport-wear">Sport wear</Link>
                 </section>
                 <br></br>
                 <section id="center">
